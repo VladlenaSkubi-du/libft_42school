3bcec8c0c64cbd2f780887172e7a0300085e3dda
Vladlena <vskubish1410@bk.ru>
2020-03-02 12:07:46 +0300

    Changes after 42sh

--- ft_arrdel.c
+++ ft_arrdel.c
@@ -6,7 +6,7 @@
 /*   By: sschmele <sschmele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 20:18:33 by sschmele          #+#    #+#             */
-/*   Updated: 2019/10/01 20:18:51 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/01/20 14:55:23 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void			ft_arrdel(char **arr)
 {
 	int			i;
 
-	if (arr == NULL)
+	if (arr == NULL || arr[0] == NULL || arr[0][0] == 0)
 		return ;
 	i = 0;
 	while (arr[i])
--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sschmele <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rbednar <rbednar@sdudent.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 19:09:42 by sschmele          #+#    #+#             */
-/*   Updated: 2018/10/31 19:15:00 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/03/01 14:53:10 by rbednar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *begin_list)
+t_list	*ft_lstlast(t_list **begin_list)
 {
-	while (begin_list->next)
-		begin_list = begin_list->next;
-	return (begin_list);
+	t_list	*tmp;
+
+	tmp = *begin_list;
+	while (tmp->next)
+		tmp = tmp->next;
+	return (tmp);
 }
