14fa7af6c9bd12a83f803bdbd7b3fcb09e42fa12
Vladlena <vskubish1410@bk.ru>
2020-03-04 15:14:28 +0300

    Changed after 42sh

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,27 +6,20 @@
 /*   By: sschmele <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 12:04:33 by sschmele          #+#    #+#             */
-/*   Updated: 2018/12/06 11:37:45 by sschmele         ###   ########.fr       */
+/*   Updated: 2020/03/04 15:12:34 by sschmele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strcmp(const char *s1, const char *s2)
+int ft_strcmp(const char *s1, const char *s2)
 {
-	int	res;
+	size_t i;
 
-	res = 0;
-	while ((*s1 != '\0') || (*s2 != '\0'))
-	{
-		res = (unsigned char)*s1 - (unsigned char)*s2;
-		if (res == 0)
-		{
-			s1++;
-			s2++;
-		}
-		else
-			break ;
-	}
-	return (res);
+	i = 0;
+	if (!s1 || !s2)
+		return (-1);
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
